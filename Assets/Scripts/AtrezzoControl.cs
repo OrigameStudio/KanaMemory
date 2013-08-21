403cd9b532470733fd609c37bf7b5b58626f26f4
Guillermo Calvo <guillermocalvo@yahoo.com>
2013-08-21 23:50:47 +0000

    Add script for interacting with atrezzo

---
+++ Assets/Scripts/AtrezzoControl.cs
@@ -0,0 +1,31 @@
+
+using UnityEngine;
+using System.Collections;
+
+
+public class AtrezzoControl : MonoBehaviour{
+
+	public float explosionForce		= 100f;
+	public float explosionRadius	= 1f;
+
+	void Update(){
+
+		if( Input.GetMouseButtonDown(0) ){
+
+			Ray rayo;
+			RaycastHit info;
+
+			rayo = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+			if( Physics.Raycast(rayo, out info) ){
+
+				if( info.collider.gameObject.tag == "ATREZZO" ){
+
+					info.rigidbody.AddExplosionForce(this.explosionForce, info.point, this.explosionRadius);
+				}
+			}
+		}
+
+	}
+
+}
