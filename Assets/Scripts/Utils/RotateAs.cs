5cf9597475d1c35183e10b93444f9c29c61b1aa4
Guillermo Calvo <guillermocalvo@yahoo.com>
2013-09-06 09:11:15 +0000

    Add script `RotateAs` (rotates an object progressively through interpolation)

---
+++ Assets/Scripts/Utils/RotateAs.cs
@@ -0,0 +1,34 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+
+public class RotateAs : MonoBehaviour{
+
+	public Quaternion	target;
+	public float		speed;
+	public bool			spherical;
+
+	void Update(){
+
+		if(spherical){
+
+			this.gameObject.transform.rotation = Quaternion.Slerp(this.gameObject.transform.rotation, this.target, this.speed);
+
+		}else{
+
+			this.gameObject.transform.rotation = Quaternion.Lerp(this.gameObject.transform.rotation, this.target, this.speed);
+		}
+	}
+
+	public static RotateAs Add(GameObject obj, Quaternion target, float speed = 0.1f, bool spherical = false){
+
+		RotateAs that = obj.AddComponent<RotateAs>();
+
+		that.target		= target;
+		that.speed		= speed;
+		that.spherical	= spherical;
+
+		return(that);
+	}
+}
