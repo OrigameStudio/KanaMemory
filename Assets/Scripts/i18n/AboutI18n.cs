5ba56cb6312d54afd3692fe317be79098a55c682
Guillermo Calvo <guillermocalvo@yahoo.com>
2013-10-04 22:35:52 +0000

    Add i18n class for *ABOUT* screen

---
+++ Assets/Scripts/i18n/AboutI18n.cs
@@ -0,0 +1,19 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+
+public class AboutI18n : MonoBehaviour{
+
+	public HUDText rateThisApp;
+
+	void Start(){
+
+		this.UpdateLanguage( MemoryGame.GetInstance().language );
+	}
+
+	public void UpdateLanguage(GameLanguageData language){
+
+		this.rateThisApp.text = language.rateThisApp;
+	}
+}
