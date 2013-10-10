b9bd2163f1437c22800b88d86411cd07328ab861
Guillermo Calvo <guillermocalvo@yahoo.com>
2013-10-10 16:51:17 +0000

    Refactor class `HUDText` (calculates text size faster)

--- Assets/Scripts/Utils/HUD/HUDText.cs
+++ Assets/Scripts/Utils/HUD/HUDText.cs
@@ -16,61 +16,242 @@ public class HUDText : HUDElement{
 	public bool				wordWrap		= true;
 	public int				shadowOffset	= 0;
 
-	public void AutoFontSize(GUIStyle style, Rect rectangle){
+	private GUIStyle currentGUIStyle = null;
+	private string currentGUIStyleId = null;
+	private string currentRectId = null;
+
+#if UNITY_EDITOR
+
+	public int				debugFontSize	= -1;
+	public int				debugFontCalls	= -1;
+	public int				debugFontFrame	= -1;
+	public string			debugGUIStyleId	= null;
+	public string			debugRectId		= null;
+	public int				debugChanges	= -1;
+
+#endif
+
+	void Start(){
+
+#if UNITY_EDITOR
+
+			this.debugChanges = 0;
+
+#endif
+
+		// Precalculate GUIStyle
+		this.SetText(this.text);
+	}
+
+	public void Prepare(){
+
+		this.GetCurrentGUIStyle( this.CalculateRectangle() );
+	}
+
+	public void SetText(string text){
+
+		this.text = text;
+
+		this.Prepare();
+	}
+
+	/*
+	private string GetGUIStyleId(Rect rectangle){
+
+		return(
+			this.text			+ "<" +
+			( this.font.family == null ? "null" : this.font.family.name)
+								+ "|" +
+			this.font.style		+ "," +
+			this.font.size		+ ">" +
+			//this.crop			+ "/" +
+			this.wordWrap		+ "@" +
+			//rectangle.x		+ "," +
+			//rectangle.y		+ ":" +
+			rectangle.width		+ "x" +
+			rectangle.height 	+ "@" +
+			Screen.width		+ "x" +
+			Screen.height
+		);
+	}
+	*/
+
+	private string GetGUIStyleId(){
+
+		return(
+			this.text			+ "<" +
+			( this.font.family == null ? "null" : this.font.family.name)
+								+ "|" +
+			this.font.style		+ "," +
+			this.font.size		+ ">" +
+			//this.crop			+ "/" +
+			this.wordWrap
+		);
+	}
+
+	private string GetRectId(Rect rectangle){
+
+		return(
+			//rectangle.x		+ "," +
+			//rectangle.y		+ ":" +
+			rectangle.width		+ "x" +
+			rectangle.height 	+ "@" +
+			Screen.width		+ "x" +
+			Screen.height
+		);
+	}
+
+	private Rect CalculateRectangle(){
+
+		return( this.padding.Pad(this.rectangle) );
+	}
+
+	private GUIStyle CalculateGUIStyle(Rect rectangle, bool sameRect){
+
+		GUIStyle style = new GUIStyle();
+
+		style.clipping = (this.crop ? TextClipping.Clip : TextClipping.Overflow);
+		style.alignment = this.alignment;
+		style.wordWrap = this.wordWrap;
+
+		if(this.font != null){
+
+			this.font.ApplyTo(style);
+
+			if(this.font.size == 0){
+
+				this.CalculateFontSize(style, rectangle, sameRect);
+			}
+
+		}
+
+		return(style);
+	}
+
+	public void CalculateFontSize(GUIStyle style, Rect rectangle, bool sameRect){
 
 		GUIContent content = new GUIContent(this.text);
 
-		int size = 1;
+		int size = (sameRect ? this.currentGUIStyle.fontSize + 1 : 1);
+		int step = (sameRect ? 4 : 16);
+
+		Debug.Log("LET'S START WITH: " + size + " @ " + step + " (" + sameRect + ")");
+
+#if UNITY_EDITOR
+
+		this.debugFontCalls = 0;
+
+#endif
 
 		if(this.wordWrap){
 
-			float guiHeight = rectangle.height;
+			float guiHeight = rectangle.height - 1;
 
-			for(style.fontSize = 1; rectangle.height >= guiHeight; size++, style.fontSize = size){
+			for(style.fontSize = size; rectangle.height > guiHeight; size += step){
 
+#if UNITY_EDITOR
+
+				this.debugFontCalls++;
+
+				Debug.Log("LET'S TRY WITH: " + size);
+#endif
+				style.fontSize = size;
 				guiHeight = style.CalcHeight(content, rectangle.width);
+			}
+
+			if(rectangle.height < guiHeight){
+
+				for(size = style.fontSize - 1; rectangle.height < guiHeight; size--){
+
+#if UNITY_EDITOR
+
+					this.debugFontCalls++;
+
+					Debug.Log("LET'S TRY AGAIN BACKWARDS WITH: " + size);
+
+#endif
+					style.fontSize = size;
+					guiHeight = style.CalcHeight(content, rectangle.width);
+				}
 			}
 
 		}else{
 
-			Vector2 guiSize = new Vector2(rectangle.width, rectangle.height);
+			Vector2 guiSize = new Vector2(rectangle.width - 1, rectangle.height - 1);
 
-			for(style.fontSize = 1; rectangle.width >= guiSize.x && rectangle.height >= guiSize.y; size++, style.fontSize = size){
+			for(style.fontSize = size; rectangle.width > guiSize.x && rectangle.height > guiSize.y; size += step){
 
+#if UNITY_EDITOR
+
+				Debug.Log("LET'S TRY WITH: " + size);
+
+				this.debugFontCalls++;
+
+#endif
+				style.fontSize = size;
 				guiSize = style.CalcSize(content);
+			}
+
+			if(rectangle.width < guiSize.x || rectangle.height < guiSize.y){
+
+				for(size = style.fontSize - 1; rectangle.width < guiSize.x || rectangle.height < guiSize.y; size--){
+
+#if UNITY_EDITOR
+
+					Debug.Log("LET'S TRY AGAIN BACKWARDS WITH: " + size);
+
+					this.debugFontCalls++;
+
+#endif
+					style.fontSize = size;
+					guiSize = style.CalcSize(content);
+				}
 			}
 		}
 
-		style.fontSize = (size - 1);
+		Debug.Log("WE FOUND: " + style.fontSize);
+	}
+
+	private GUIStyle GetCurrentGUIStyle(Rect rectangle){
+
+		string styleId	= this.GetGUIStyleId();
+		string rectId	= this.GetRectId(rectangle);
+
+		bool sameRect = ( this.currentRectId != null && this.currentRectId.Equals(rectId) );
+
+		if(this.currentGUIStyle == null || !this.currentGUIStyleId.Equals(styleId) || !sameRect){
+
+			this.currentGUIStyle	= this.CalculateGUIStyle(rectangle, sameRect);
+			this.currentGUIStyleId	= styleId;
+			this.currentRectId		= rectId;
+
+#if UNITY_EDITOR
+
+			this.debugChanges++;
+			this.debugFontFrame		= Time.frameCount;
+			this.debugFontSize		= this.currentGUIStyle.fontSize;
+			this.debugGUIStyleId	= this.currentGUIStyleId;
+			this.debugRectId		= this.currentRectId;
+
+#endif
+		}
+
+		return(this.currentGUIStyle);
 	}
 
 	void OnGUI(){
 
 		if( !this.Applies() || this.doNotRender ) return;
 
+		Rect position;
+		GUIStyle style;
+
 		GUI.depth = this.guiDepth;
 
 		this.DebugOnGUI();
 
-		Rect position = this.rectangle;
-
-		position = this.padding.Pad(position);
-
-		GUIStyle style = new GUIStyle();
-
-		if(this.font != null){
-
-			this.font.ApplyTo(style);
-		}
-
-		style.clipping = (this.crop ? TextClipping.Clip : TextClipping.Clip);
-		style.alignment = this.alignment;
-		style.wordWrap = this.wordWrap;
-
-		if(this.font != null && this.font.size == 0){
-
-			this.AutoFontSize(style, position);
-		}
+		position	= this.CalculateRectangle();
+		style		= this.GetCurrentGUIStyle(position);
 
 		if(this.shadowOffset != 0){
 
