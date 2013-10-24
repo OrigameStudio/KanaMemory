d91dddba3bfab284c8e0f71e096213f91921b4da
Guillermo Calvo <guillermocalvo@yahoo.com>
2013-10-24 23:41:38 +0000

    Store statistics about games played/won/lost as app data
    This data may be used to determine if the player plays the game often.

--- Assets/Scripts/MemoryGame.cs
+++ Assets/Scripts/MemoryGame.cs
@@ -5,6 +5,10 @@ using System.Collections;
 
 public class MemoryGame : MonoBehaviour{
 
+	public static string TOTAL_GAMES	= "TotalGames";
+	public static string GAMES_WON		= "GamesWon";
+	public static string GAMES_LOST		= "GamesLost";
+
 	public GameLanguageData		language;
 	public GameStatus			status		= GameStatus.Ready;
 	public GameType				type		= GameType.GameType1;
@@ -13,11 +17,13 @@ public class MemoryGame : MonoBehaviour{
 	public bool					hint		= false;
 	public MemoryGameStatsCards	cards;
 	public int					games;
+	public int					gamesWon;
+	public int					gamesLost;
 	public MemoryGameStatsTime	time;
 
 	private static MemoryGame instance;
 	public static MemoryGame GetInstance(){
-		
+
 		if(MemoryGame.instance == null){
 
 			MemoryGame.instance = (MemoryGame)GameObject.FindObjectOfType( typeof(MemoryGame) );
@@ -38,13 +44,16 @@ public class MemoryGame : MonoBehaviour{
 
 			this.language = ( GameAvailableLanguages.FindInstance() ).GetGameLanguage().data;
 
+			this.games		= PlayerPrefs.GetInt(TOTAL_GAMES);
+			this.gamesWon	= PlayerPrefs.GetInt(GAMES_WON);
+			this.gamesLost	= PlayerPrefs.GetInt(GAMES_LOST);
+
 			GameObject.DontDestroyOnLoad(this.gameObject);
 		}
 	}
 
 	public void StartGame(int pairs, int seconds){
 
-		this.games++;
 		this.time.Reset(seconds);
 		this.cards.Reset(pairs);
 
@@ -53,11 +62,16 @@ public class MemoryGame : MonoBehaviour{
 
 	public void UpdateTimeLeft(float now){
 
+		if(this.status != GameStatus.Playing){
+
+			return;
+		}
+
 		this.time.left = this.time.total - Mathf.FloorToInt(now - this.time.start);
 
 		if(this.time.left <= 0 && this.PairsLeft() > 0){
 
-			this.status = GameStatus.Failure;
+			this.Fail();
 		}
 	}
 
@@ -80,6 +94,11 @@ public class MemoryGame : MonoBehaviour{
 
 		bool success;
 
+		if(this.status != GameStatus.Playing){
+
+			return(true);
+		}
+
 		success = card1.Matches(card2);
 
 		if(success){
@@ -88,7 +107,7 @@ public class MemoryGame : MonoBehaviour{
 
 			if(this.PairsLeft() == 0){
 
-				this.status = GameStatus.Success;
+				this.Success();
 			}
 
 		}else{
@@ -97,5 +116,28 @@ public class MemoryGame : MonoBehaviour{
 		}
 
 		return(success);
+	}
+
+	private void Fail(){
+
+		this.status = GameStatus.Failure;
+
+		this.games++;
+		PlayerPrefs.SetInt(TOTAL_GAMES, this.games);
+
+		this.gamesLost++;
+		PlayerPrefs.SetInt(GAMES_LOST, this.gamesLost);
+	}
+
+
+	private void Success(){
+
+		this.status = GameStatus.Success;
+
+		this.games++;
+		PlayerPrefs.SetInt(TOTAL_GAMES, this.games);
+
+		this.gamesWon++;
+		PlayerPrefs.SetInt(GAMES_WON, this.gamesWon);
 	}
 }
